94d7b7790aa78b33aae010ba68c390f68f61149b
Jakub Rybarski <jakubrybarski21@gmail.com>
2025-05-08 21:00:02 +0200

    adding switch between dark and light mode

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector(".container");
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+const sunIcon = document.querySelector(".icon.sun");
+const moonIcon = document.querySelector(".icon.moon");
 
 async function loadQuestions() {
   try {
@@ -17,6 +21,33 @@ async function loadQuestions() {
   }
 }
 
+function updateIcons(isDarkMode) {
+  if (isDarkMode) {
+    sunIcon.src = "assets/images/icon-sun-dark.svg";
+    moonIcon.src = "assets/images/icon-moon-dark.svg";
+    container.style.backgroundImage = container.style.backgroundImage =
+      "url('/assets/images/pattern-background-mobile-dark.svg')";
+  } else {
+    sunIcon.src = "assets/images/icon-sun-light.svg";
+    moonIcon.src = "assets/images/icon-moon-light.svg";
+    container.style.backgroundImage =
+      "url('/assets/images/pattern-background-mobile-light.svg')";
+  }
+}
+
+if (themeToggle.checked) {
+  body.classList.add("dark-mode");
+  updateIcons(true);
+} else {
+  updateIcons(false);
+}
+
+themeToggle.addEventListener("change", () => {
+  const isDarkMode = themeToggle.checked;
+  body.classList.toggle("dark-mode", isDarkMode);
+  updateIcons(isDarkMode);
+});
+
 function generateStart(quizzes) {
   const title1 = document.createElement("h1");
   title1.innerHTML = "Welcome to the";
@@ -29,9 +60,9 @@ function generateStart(quizzes) {
   container.appendChild(title2);
   container.appendChild(title3);
 
-  quizzes.forEach((quiz) => {
+  quizzes.forEach((quiz, index) => {
     const answer = document.createElement("div");
-    answer.classList.add("quiz-option");
+    answer.classList.add("quiz-option", `option-${index + 1}`);
 
     const answerImage = document.createElement("img");
     answerImage.src = quiz.icon;
